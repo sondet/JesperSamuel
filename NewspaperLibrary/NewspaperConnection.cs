1653c0a20442ba728ea1b7f92825325fb70fe5af
Jesper Sundin <jesperlol123@gmail.com>
2013-02-09 18:47:33 +0100

    Add Newspaper class

---
+++ NewspaperLibrary/NewspaperConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewspaperLibrary
+{
+    public class NewspaperConnection
+    {
+    }
+
+    public class Newspaper
+    {
+        private Uri mUri;
+        public Uri Uri
+        {
+            get { return mUri; }
+            set { mUri = value; }
+        }
+
+        private string mName;
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        
+    }
+}
+
