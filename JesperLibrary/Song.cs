b68f62e492f92f378db2825e44b53d025aa294a6
Jesper Sundin <jesperlol123@gmail.com>
2013-02-10 01:57:06 +0100

    First implementation of Song

--- JesperLibrary/Song.cs
+++ JesperLibrary/Song.cs
@@ -8,5 +8,25 @@ namespace MelloLibrary
 {
     class Song
     {
+        public Artist Artist { get; set; }
+        public string Title { get; set; }
+
+        public Song()
+        {
+            Artist = new Artist();
+            Title = "No titel";
+        }
+
+        public Song(Artist artist, string title)
+        {
+            if (title == null || artist == null) throw new ArgumentNullException();
+            Artist = artist;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return Title.ToString() + " :: " + Artist.ToString();
+        }
     }
 }
