db2a4680a466ae61bd865e46381dc28a05be004f
Jesper Sundin <jesperlol123@gmail.com>
2013-02-10 01:51:26 +0100

    Add Song class

--- JesperLibrary/Artist.cs
+++ JesperLibrary/Artist.cs
@@ -8,5 +8,26 @@ namespace MelloLibrary
 {
     class Artist
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Artist()
+        {
+            FirstName = "No first name";
+            LastName = "No last name";
+        }
+
+        public Artist(string firstName, string lastName)
+        {
+            if (firstName == null || lastName == null) throw new ArgumentNullException();
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return FirstName.ToString() + LastName.ToString();
+        }
+
     }
 }
