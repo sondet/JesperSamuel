254d423574d158cff5dfe4b9c61479a1eb226e1e
Jesper Sundin <jesperlol123@gmail.com>
2013-02-14 18:29:57 +0100

    Add SiteSearcher and WebSite classes

---
+++ WebLibrary/SiteSearcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WebLibrary
+{
+    /// <summary>
+    /// Class that searches a site for keywords
+    /// </summary>
+    public class SiteSearcher
+    {
+        
+
+        public SiteSearcher(Uri url)
+        {
+
+        }
+
+        public SiteSearcher(string url)
+        {
+
+        }
+    }
+}
---
+++ WebLibrary/WebSite.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLibrary
+{   
+    /// <summary>
+    /// A class representing a website
+    /// </summary>
+    class WebSite
+    {
+        private Uri _Uri;
+        /// <summary>
+        /// Uri of the <seealso cref="WebSite"/>
+        /// </summary>
+        public Uri Uri { get { return _Uri; } }
+        
+        public WebSite(string uri)
+        {
+            try
+            {
+                _Uri = new Uri(uri);
+            }
+            catch (UriFormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
