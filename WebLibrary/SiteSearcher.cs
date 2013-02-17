19bdd76b340fa3a3748d123166d3acc7788c98a0
Jesper Sundin <jesperlol123@gmail.com>
2013-02-17 21:27:12 +0100

    Started implementing SiteSearcher

--- WebLibrary/SiteSearcher.cs
+++ WebLibrary/SiteSearcher.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace WebLibrary
 {
@@ -14,14 +15,43 @@ namespace WebLibrary
     {
         
 
-        public SiteSearcher(Uri url)
+        public SiteSearcher()
         {
 
         }
 
-        public SiteSearcher(string url)
+        public SiteSearchResult SearchSiteForWord(WebSite webSite, string word)
         {
+            if (webSite == null) throw new ArgumentNullException("webSite");
+            if (webSite == null) throw new ArgumentNullException("word");
 
+            string rawHtml = webSite.RawContent;
+            if (string.IsNullOrWhiteSpace(rawHtml))
+                throw new ArgumentException("WebSite object had no raw content");
+
+            Regex rgx = new Regex(word);
+
+            MatchCollection matches = rgx.Matches(rawHtml);
+            if (matches.Count > 0)
+            {
+                return new SiteSearchResult(webSite,word) {Occurences = matches.Count};
+            }
+            return new SiteSearchResult(webSite,word) {Occurences = 0};
+        }
+
+    public class SiteSearchResult
+    {
+        public WebSite WebSiteSearched { get; set; }
+
+        public string Word { get; set; }
+
+        public int Occurences { get; set; }
+
+        public SiteSearchResult(WebSite siteSearched, string forWord)
+        {
+            this.WebSiteSearched = siteSearched;
+            this.Word = forWord;
         }
     }
+
 }
